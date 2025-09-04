b7757e00132946e90cf8a09305ef12e90517f89d
AndreVictordmr <andre.victordmr@gmail.com>
2025-09-04 16:22:32 -0300

    Integração HTML e PHP

--- hello_world.php
+++ hello_world.php
@@ -1,9 +1,25 @@
-<?php 
-    //Comentario em linha
-    /* Comentario bloco */
-    
-    //Comando de saida
-    echo "Olá mundo";
-    
-    
-?>
+<!DOCTYPE html>
+<html lang="pt-br">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Introdução a PHP</title>
+    </head>
+    <body>
+        <?php
+            /*
+            Comando para saiada de dados 
+            echo "<h1> Titulo </h1>";
+            print "texto";
+            */
+
+            //
+        ?>
+
+        <!-- ?= subistitui o echo(usado somente quando somente eu quero ter a saiada unica) -->
+        <p> <?= "ola"?></p>
+
+      
+
+    </body>
+</html>
