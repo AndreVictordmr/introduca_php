efcd202c44624274a5821a19d1a8d204b1863070
AndreVictordmr <41845560+AndreVictordmr@users.noreply.github.com>
2025-09-06 12:24:06 -0300

    exercio pagina individual

--- exercicio.php
+++ exercicio.php
@@ -1,4 +1,32 @@
-<!doctype HTML>
-<html>
+<!DOCTYPE html>
+<html lang="pt-br">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Pedido</title>
+    </head>
+    <body>
+        <?php
+            //Exercicio
+            $nome = "Hermione Granger";
+            $idade = 17;
+            $especie = "humano";
+            $altura = 1.65;
+            $hobbies = ["ler","estudar",""]
 
+            echo "<p>Bruxo : $nome <br>";
+            echo "Idade : $idade <br>";
+            echo "Especie : ".$especie . "<br>";
+            if($isAlive){
+                echo "Vivo : sim <br>";
+            }else{
+                echo "Vivo : não <br>";
+            }
+            echo "Altura : $altura <br>"
+            echo "Hobbies : $hobbies </p>"
+            
+
+
+        ?>
+    </body>
 </html>
